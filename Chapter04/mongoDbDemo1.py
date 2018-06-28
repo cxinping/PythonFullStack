63d3790494a592f0f959460e79268adcc2b2e2e8
xpws2006@163.com <123@welcome>
2018-06-29 03:29:15 +0800

    Python  处理MongoDB

--- Chapter04/mongoDbDemo1.py
+++ Chapter04/mongoDbDemo1.py
@@ -1,8 +1,50 @@
 # -*- coding: utf-8 -*-
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+
 #conn = MongoClient('192.168.1.9',27017)
 conn = MongoClient('mongodb://192.168.1.9:27017/')
 
 db = conn.mydb
+# 插入一条文档
 #rs =db.users.insert({"name":'xinping','province':'北京','age':25})
 #print('user insert:{0}'.format(rs))
+
+#插入多条文档
+'''
+rs = db.users.insert([
+    {"name":'user1','province': '天津', 'age' : 25},
+    {"name":'user2','province': '北京','age' : 24},
+    {"name":'user3','province': '哈尔滨', 'age' : 25}
+])
+print('Multiple users: {0}'.format(rs ))
+'''
+
+#查询一条文档
+#rs = db.users.find_one({"name" : "user1"})
+#rs = db.users.find_one({'age':{"$lt":25}})
+#print(rs)
+#print('name={0} ,province={1} ,age={2}'.format(rs['name'],rs['province'],rs['age']))
+
+#查询多条文档
+'''
+for item in db.users.find({"age" : 25 }):
+    print(item)
+'''
+
+#条件查询
+#count = db.users.find({"age":{"$lt":25}} ).count()
+#print('count={0}'.format(count))
+
+# 根据_id查询记录
+#rs = db.users.find_one({'_id':ObjectId('5b35214d1ec3b721f00d31fc')})
+#print(rs)
+
+# 结果排序
+'''
+for item in db.users.find().sort("age"):
+    print(item)
+'''
+import pymongo
+for item in db.users.find().sort("age", pymongo.DESCENDING):
+    print(item)
