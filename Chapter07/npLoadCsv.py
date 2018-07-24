fc0c78cce266a41a7eb4e619542f8e541608fade
xpws2006@163.com <123@welcome>
2018-07-25 06:13:14 +0800

    Numpy读取文件

---
+++ Chapter07/npLoadCsv.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+arr2 = np.loadtxt('test1.csv' , delimiter=',')
+print(arr2)
+
---
+++ Chapter07/npLoadTxt.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+arr1= np.loadtxt('test1.txt' ,delimiter=',')
+print(arr1)
+
