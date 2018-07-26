909dc8a79676a3db13d0a08c7c57cb15f517d05b
xpws2006@163.com <123@welcome>
2018-07-27 04:19:06 +0800

    优化代码

--- Chapter07/sql_saveData.py
+++ Chapter07/sql_saveData.py
@@ -3,11 +3,12 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 # 初始化引擎
-engine = create_engine('mysql+pymysql://root:123456@192.168.1.9:3306/mytestdb?charset=utf8')
+engine = create_engine('mysql+pymysql://root:123456@127.0.0.1:3306/mytestdb?charset=utf8')
 
 sdata1 = {'name' : ['王五1','王五2'], 'age' : [20, 21], 'sex':['female','male'] , 'income':[5000,6000]   }
 df1 = pd.DataFrame(sdata1 )
 
-# 配合pandas的to_sql方法使用十分方便（dataframe对象直接入库）
-df1.to_sql('employee2', con=engine, if_exists='append')
-print( df1 )
+# 使用Pandas的to_sql方法将DataFrame对象直接入库
+df1.to_sql('employee', con=engine, if_exists='append')
+
+
