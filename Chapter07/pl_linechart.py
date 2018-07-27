4b337802b91e2881740bb4e88a0595cd29a38151
xpws2006@163.com <123@welcome>
2018-07-28 02:56:57 +0800

    优化代码

--- Chapter07/pl_linechart.py
+++ Chapter07/pl_linechart.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-#导入 pyplot模块，并起一个别名 plt
+# 导入 pyplot模块，并起一个别名 plt
 import matplotlib.pyplot as plt
 # 设置X轴 的坐标为 [1,2,3]， 设置Y轴的坐标为 [3,2,1]
 plt.plot( [1,2,3],[3,2,1] )
+# 画图
 plt.show()
+
