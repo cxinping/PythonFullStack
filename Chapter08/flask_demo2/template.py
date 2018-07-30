2d1848ace71997a34c0881fd83d7cf4ad44a388c
xpws2006@163.com <123@welcome>
2018-07-31 03:08:12 +0800

    模板渲染

---
+++ Chapter08/flask_demo2/template.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=80)
