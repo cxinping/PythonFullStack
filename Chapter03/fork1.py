39917884bc1f1c24ebba42190eda4674b05c5fe7
xpws2006@163.com <123@welcome>
2018-08-11 12:38:03 +0800

    Linux平台下的多进程

---
+++ Chapter03/fork1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import time
+
+print('fork1')
+
+time.sleep(60)
+
+
+
---
+++ Chapter03/fork2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import os
+import time
+
+pid = os.fork()
+print(type(pid))
+print(pid)
+
+if pid == 0 :
+   print("子进程(pid={0}),对应的父进程id={1})".format(os.getpid(),os.getppid()))
+
+else:
+    print('父进程(pid={0}),生成了子进程(cpid={1})'.format(os.getpid(), pid))
+
+time.sleep(60)
