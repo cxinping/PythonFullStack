9cb1e0d0202cf748712ff281e32b2c9ac0f0e9af
xpws2006@163.com <123@welcome>
2018-08-11 15:58:50 +0800

    跨平台的多进程

---
+++ Chapter03/mp1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from multiprocessing import Process
+import os
+import time
+
+def handle(name):
+    print('name={0},pid={1},ppid={2}'.format(name, os.getpid(),os.getppid()))
+
+if __name__ == '__main__':
+    p1 = Process(target=handle, args=('python',))
+    p2 = Process(target=handle, args=('java',))
+    p1.start()
+    p2.start()
+    # 等待当前进程完，在执行主进程
+    p1.join()
+    p2.join()
+    print('multiprocess')
