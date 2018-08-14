8d40f8ade3c729941435e11c72b034172b4688c3
xpws2006@163.com <123@welcome>
2018-08-14 14:59:22 +0800

    多线程

--- Chapter03/threadDemo01.py
+++ Chapter03/threadDemo01.py
@@ -11,10 +11,10 @@ def printNum(idx):
         time.sleep(delay)
 
 if __name__ == '__main__':
-    t1 = threading.Thread(target=printNum, args=(2,),name="thread1" )
-    t2 = threading.Thread(target=printNum, args=(3,),name="thread2")
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
+    th1 = threading.Thread(target=printNum, args=(2,)  )
+    th2 = threading.Thread(target=printNum, args=(3,) )
+    th1.start()
+    th2.start()
+    th1.join()
+    th2.join()
     print("{0} 线程结束".format(threading.current_thread().getName()))
