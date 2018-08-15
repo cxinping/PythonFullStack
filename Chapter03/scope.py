94f26004a132286c41ac5dc594612552a5f30d55
xpws2006@163.com <123@welcome>
2018-08-15 12:00:10 +0800

    多线程例子

---
+++ Chapter03/scope.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# 全局变量
+balance = 1
+
+def change():
+    # 定义全局变量
+    global balance
+    balance = 100
+    print("change() balance={0}".format(balance) )
+
+if __name__ == "__main__" :
+    change()
+    print("修改后的 balance={0}".format(balance) )
