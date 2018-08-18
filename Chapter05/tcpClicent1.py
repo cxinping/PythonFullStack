9b75043ea674f3ef7d900f5c923c98f44caea0fc
xpws2006@163.com <123@welcome>
2018-08-18 23:46:49 +0800

    tcp协议的网络连接

--- Chapter05/tcpClicent1.py
+++ Chapter05/tcpClicent1.py
@@ -5,10 +5,10 @@ from socket import socket
 socket1 = socket()
 port = 12345
 
-socket1.connect(("127.0.0.1", port))
+socket.connect(("127.0.0.1", port))
 print('start Socket client ...')
-msg = socket1.recv(1014).decode('utf-8')
+msg = socket.recv(1014).decode('utf-8')
 print("client receive message={0}".format(msg))
-socket1.close()
+socket.close()
 
 
--- Chapter05/tcpServer1.py
+++ Chapter05/tcpServer1.py
@@ -9,13 +9,15 @@ socket1.bind(("127.0.0.1", port) )
 
 # 设置最大连接数
 socket1.listen(10)
-# 建立客户端连接
-clientSocket,addr  = socket1.accept()
-print("新连接:",addr )
-msg = 'socket test'
 
-print('start Socket Server ...')
-clientSocket.send(msg.encode('utf'))
-clientSocket.close()
-socket1.close()
+while True:
+    # 建立客户端连接
+    clientSocket,addr  = socket1.accept()
+    print("新连接:",addr )
+    msg = 'socket test'
+
+    print('start Socket Server ...')
+    clientSocket.send(msg.encode('utf'))
+    clientSocket.close()
+    #socket1.close()
 
