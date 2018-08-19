1b9316fe10882da2a40933df064b9aaf1f68fc4c
xpws2006@163.com <123@welcome>
2018-08-20 02:30:38 +0800

    优化发送文件Socket

--- Chapter05/fileClient1.py
+++ Chapter05/fileClient1.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import socket
-import os
-import sys
 
 cSocket = socket.socket()
 cSocket.connect(('127.0.0.1', 5000))
-content = cSocket.recv(1024 * 1024 * 5)  #配置5M缓存
-with open('photo3.jpg', 'wb') as file:
+# 配置5M缓存
+content = cSocket.recv(1024 * 1024 * 5)
+#content = cSocket.recv(1024 * 50 )
+
+with open('photo2.jpg', 'wb') as file:
     file.write(content)
 
 print('===recv file ok===')
