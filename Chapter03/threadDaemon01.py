cef2ba36a89080b42aab8598b6ccae53b8b31f4b
xpws2006@163.com <123@welcome>
2018-08-19 20:04:06 +0800

    守护线程

--- Chapter03/threadDaemon01.py
+++ Chapter03/threadDaemon01.py
@@ -5,7 +5,7 @@ import threading, time
 def run(taskName):
     print("任务:", taskName)
     time.sleep(2)
-    print("{0} 任务执行完毕".format(taskName))  # 查看每个子线程
+    print("{0} 任务执行完毕".format(taskName))
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -14,5 +14,7 @@ if __name__ == '__main__':
         thr.start()
 
     # 查看主线程和当前活动的所有线程数
-    print("{0}线程结束，当线程数量={1}".format( threading.current_thread().getName(), threading.active_count()))
+    thrName = threading.current_thread().getName()
+    thrCount = threading.active_count()
+    print("{0}线程结束，当线程数量={1}".format(thrName, thrCount))
     print("消耗时间:", time.time() - start_time)
