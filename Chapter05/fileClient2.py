cbcfa4f40c03a2df374095b7cc47e12e46ebfc1c
xpws2006@163.com <123@welcome>
2018-08-20 01:45:34 +0800

    优化发送文件Socket

--- Chapter05/fileClient2.py
+++ Chapter05/fileClient2.py
@@ -7,11 +7,28 @@ import sys
 cSocket = socket.socket()
 cSocket.connect(('127.0.0.1', 5000))
 
+# Step1： 获得接收文件的大小
 serverResponse =  cSocket.recv(1024).decode('utf8')
 fileTotalSize = int(serverResponse)
 print(fileTotalSize,type(fileTotalSize))
-#with open('photo3.jpg', 'wb') as file:
+# Step2：发送确认信息
+cSocket.send("ready to recv file".encode("utf8"))
+# 初始化接收大小
+revivedSize = 0
+fileName = 'photo3.jpg'
+with open(fileName, 'wb') as file:
 #    file.write(content)
-
+    count = 0
+    # 判断接收大小和文件大小比较
+    while revivedSize < fileTotalSize:
+        # 配置1M缓存
+        data = cSocket.recv(1024 * 1024)
+        # 接收文件大小
+        revivedSize = revivedSize + len(data)
+        print("revivedSize={0},count={1}，data={2}".format(revivedSize,count,len(data)) )
+        count = count + 1
+        file.write(data)
+    else:
+        pass
 print('===recv file ok===')
 
--- Chapter05/fileServer2.py
+++ Chapter05/fileServer2.py
@@ -6,15 +6,23 @@ sSocket = socket.socket()
 sSocket.bind(('127.0.0.1', 5000))
 sSocket.listen(10)
 
-fileName = 'photo.jpg'
+fileName = 'phtot.jpg'
 while True:
     cSocket, addr = sSocket.accept()
-    with open('photo.jpg', 'rb') as file:
-        fileSize = os.stat(fileName).st_size  # 获取文件大小
+    with open(fileName, 'rb') as file:
+        # Step1： 获取文件大小
+        fileSize = os.stat(fileName).st_size
+        # Step2： 发送文件大小给客户端
         cSocket.send(str(fileSize).encode("utf8"))
+        data = cSocket.recv(1024).decode("utf8")
+        print("step2:",data)
         #content = file.read()
         #cSocket.send(content)
+
+        # Step3: 边读文件边发送数据
         for line in file:
-            print(line)
+            cSocket.send(line)
+            print("send data length=>{0}".format(len(line)))
+
     print('===send file ok===')
 
