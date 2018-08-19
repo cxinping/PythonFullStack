76f9d03a01406cd8752353cc9301966cf60848fc
xpws2006@163.com <123@welcome>
2018-08-19 23:41:48 +0800

    socket 发送文件

---
+++ Chapter05/fileServer1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+sSocket = socket.socket()
+sSocket.bind(('127.0.0.1', 5000))
+sSocket.listen(10)
+
+while True:
+    cSocket, addr = sSocket.accept()
+    with open('photo.jpg', 'rb') as file:
+        content = file.read()
+    cSocket.send(content)
+    print('===send file ok===')
+
