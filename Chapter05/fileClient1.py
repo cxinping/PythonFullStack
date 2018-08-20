ac9161b78e74b436f9388a5cf2a66fcd0dfd9b24
xpws2006@163.com <123@welcome>
2018-08-20 16:13:24 +0800

    优化发送文件Socket

--- Chapter05/fileClient1.py
+++ Chapter05/fileClient1.py
@@ -2,7 +2,7 @@
 
 import socket
 
-cSocket = socket.socket()
+cSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cSocket.connect(('127.0.0.1', 5000))
 # 配置5M缓存
 content = cSocket.recv(1024 * 1024 * 5)
--- Chapter05/fileServer1.py
+++ Chapter05/fileServer1.py
@@ -2,7 +2,7 @@
 
 import socket
 
-sSocket = socket.socket()
+sSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sSocket.bind(('127.0.0.1', 5000))
 sSocket.listen(10)
 
