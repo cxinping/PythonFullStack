f6ed7c6a15eaf0e4a3b47820d73a2a99daf310c2
xpws2006@163.com <123@welcome>
2018-08-22 22:47:32 +0800

    多线程与网络编程

--- Chapter05/tcpChatClient.py
+++ Chapter05/tcpChatClient.py
@@ -4,26 +4,34 @@ import threading
 import socket
 import os
 import sys
+import time
 
 cSocket = socket.socket()
 cSocket.connect(('127.0.0.1', 5000))
 msg = ' '
+running = True
 
 def recvMsg():
-    global msg
-    while msg:
+    global running
+
+    while running:
+        print("***** running=[{0}]".format(running) )
         msg_bytes = cSocket.recv(1024)
-        msg = msg_bytes.decode('utf-8')
-        if msg != 'exit!':
-            print('\n%s\t%s' % ('msg:', msg))
+        msg = msg_bytes.decode('utf8')
+        print('\n从服务器端接收的消息: {0}\n'.format(msg) )
+        time.sleep(1)
 
 def sendMsg():
-    global msg
-    while msg:
+    global running
+    while running:
         # print('msg')
-        msg = input('请输入要发送给对方的消息：')
-        msg_bytes = msg.encode('utf-8')
+        msg = input('\n请输入要发送给服务器的消息：')
+        msg_bytes = msg.encode('utf8')
         cSocket.send(msg_bytes)
+
+        if msg == "exit":
+            running = False
+            break
 
 if __name__ == '__main__':
     t1 = threading.Thread(target=recvMsg, args=(), name='recv_thread')
--- Chapter05/tcpChatServer.py
+++ Chapter05/tcpChatServer.py
@@ -3,27 +3,38 @@
 import socket
 import threading        # 使用多线程
 
-sSocket = socket.socket()
-sSocket.bind(('127.0.0.1', 5000))
-sSocket.listen(10)
-cSocket, addr = sSocket.accept()
+def main():
+    # 创建socket对象。调用socket构造函数
+    sSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 将socket绑定到指定地址，第一个参数为ip地址，第二个参数为端口号
+    sSocket.bind(('127.0.0.1', 5000))
+    # 设置最多连接数量
+    sSocket.listen(10)
+    print('开始启动服务器 ...')
 
-def sendMsg():
     while True:
-        # print('msg')
-        msg = input('\ninput your message to client:')
-        msg_bytes = msg.encode('utf-8')
-        cSocket.send(msg_bytes)
-        print('%s\t%s' % ('msg', msg))
+        # 服务器套接字通过socket的accept方法等待客户请求一个连接
+        cSocket, address = sSocket.accept()
+        print("客户端{0} 连接到服务器".format(address))
+        thr1 = threading.Thread(target=recvMsg, args=(cSocket, address))
+        thr1.start()
 
-def recvMsg():
+def recvMsg(cSocket,addr):
     while True:
         msg_bytes = cSocket.recv(1024)
-        msg = msg_bytes.decode('utf-8')
-        print('%s\t%s' % ('msg', msg))
+        msg = msg_bytes.decode('utf8')
+        print('从{0}客户端收到消息：{1}'.format(addr,msg) )
+        if not msg or  msg == "exit":
+            print("关闭客户端{0}连接".format(addr))
+            cSocket.close()
+            break
+
+        # 将接收到的信息原样的返回到客户端中
+        cSocket.send(msg_bytes)
 
 if __name__ == '__main__':
-    t1 = threading.Thread(target=recvMsg, args=(), name='recv_thread')
-    t2 = threading.Thread(target=sendMsg, args=(), name='send_thread')
-    t1.start()
-    t2.start()
+    #t1 = threading.Thread(target=recvMsg, args=(), name='recv_thread')
+    #t2 = threading.Thread(target=sendMsg, args=(), name='send_thread')
+    #t1.start()
+    #t2.start()
+    main()
