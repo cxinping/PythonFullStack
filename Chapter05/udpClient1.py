e5c4c16e0b857e92d5e62461e1ea51f07b9cd006
xpws2006@163.com <123@welcome>
2018-08-26 17:50:15 +0800

    多线程与网络编程

--- Chapter05/udpClient1.py
+++ Chapter05/udpClient1.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import socket
+
 HostPort = ('127.0.0.1',7777)
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+udpClisock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 while True:
     user_input = input('请输入要发送给对方的消息：')
     if user_input == 'quit':
         break
     # 指定地址端口发送数据，数据必须encode
-    sock.sendto(user_input.encode('utf8'),HostPort)
-sock.close()
+    udpClisock.sendto(user_input.encode('utf8'),HostPort)
+    data,addr = udpClisock.recvfrom(1024)
+    msg = data.decode("utf8")
+    print("客户端 接收消息 {0}".format(msg))
+
+udpClisock.close()
--- Chapter05/udpServer1.py
+++ Chapter05/udpServer1.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import socket
+
 HostPort = ('127.0.0.1',7777)
 #创建UDP套接字
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+udpSerSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 #服务器端绑定端口
-sock.bind(HostPort)
+udpSerSock.bind(HostPort)
 
 while True:
-    # 接收端口数据
-    data,addr = sock.recvfrom(1024)
+    # 接收数据和端口
+    data,addr = udpSerSock.recvfrom(1024)
     # 打印数据
-    print("接收消息：",data.decode('utf8'))
+    print("服务器 接收消息：",data.decode('utf8'))
+    sendMsg = "hello {0}".format(data.decode('utf8'))
+    udpSerSock.sendto(sendMsg.encode("utf8") , addr)
