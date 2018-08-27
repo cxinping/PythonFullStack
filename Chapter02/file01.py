88382df04e3e3c1009daa4961164d8f6ca75d648
xpws2006@163.com <123@welcome>
2018-08-27 21:39:00 +0800

    拷贝文件

---
+++ Chapter02/file01.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# 以二进制格式打开一个文件用于读写
+file = open('photo.jpg', 'rb' )
+content = file.read()
+file.close()
+
+# 以二进制格式打开一个文件只用于写入
+file2 = open("photo2.jpg", "wb")
+file2.write(content)
+file2.close()
+
