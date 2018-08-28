64d64a11cfca0c788e7c89926735c6a616cfa6b0
xpws2006@163.com <123@welcome>
2018-08-28 09:10:51 +0800

    操作csv文件。

--- Chapter02/csv02.py
+++ Chapter02/csv02.py
@@ -2,7 +2,7 @@
 
 import csv
 
-with open('test2.csv' ,'w') as file:
+with open('test2.csv' ,'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['a','b', 'c'] )
     writer.writerow([1,2,3] )
