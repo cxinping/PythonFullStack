255d5d81331cd264c424179964df2854a1769def
xpws2006@163.com <123@welcome>
2018-08-31 00:59:11 +0800

    导入包的例子

---
+++ Chapter02/packageDemo/aaa/pm1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# 配置模块的变量
+lang = 'python'
+
+# 配置模块的函数
+def sayHello():
+    print('hello ' + lang)
---
+++ Chapter02/packageDemo/bbb/callPm1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#导入包下的模块
+import aaa.pm1 as pm
+
+# 调用模块的函数
+pm.sayHello()
+
+# 调用模块下的变量
+print("调用模块的变量 pm.lang={0}".format(pm.lang))
+
+
+
