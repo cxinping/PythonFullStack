a4af561ac5b51e5a7a3ee065a5583def27e31b06
xpws2006@163.com <123@welcome>
2018-08-31 04:01:31 +0800

    高阶函数例子

---
+++ Chapter03/lambdaDemo01.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# lambda匿名函数
+sayHello = lambda: print("hello ,python")
+# 调用匿名函数
+sayHello()
---
+++ Chapter03/mapDemo01.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+def fun(x):
+    return x * x
+
+result = map(fun , [1,2,3])
+print(list( result) )
