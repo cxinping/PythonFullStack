d2f47e5b214ea95086e8b0e3a84dfa325a59251c
xpws2006@163.com <123@welcome>
2018-08-31 20:51:09 +0800

    生成器例子

--- Chapter03/gen01.py
+++ Chapter03/gen01.py
@@ -2,17 +2,16 @@
 
 def mygen():
     print('gen() 1')
-    yield 1
+    yield 1  # 返回第一个值
 
     print('gen() 2')
-    yield 2
+    yield 2  # 返回第二个值
 
     print('gen() 3')
-    yield 3
+    yield 3  # 返回第三个值
 
-
-gen = mygen()
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
+gen = mygen()  # 拿到一个生成器
+print(next(gen))  # 取第一个值
+print(next(gen))  # 取第二个值
+print(next(gen))  # 取第三个值
+print(next(gen))  # 取第不存在的第四个值，抛出异常
