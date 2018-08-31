4bf3277435157e78ba1dd3d38c77a75aa03eb85b
xpws2006@163.com <123@welcome>
2018-08-31 20:53:57 +0800

    生成器例子

--- Chapter03/gen01.py
+++ Chapter03/gen01.py
@@ -10,8 +10,8 @@ def mygen():
     print('gen() 3')
     yield 3  # 返回第三个值
 
-gen = mygen()  # 拿到一个生成器
+gen = mygen()     # 拿到一个生成器
 print(next(gen))  # 取第一个值
 print(next(gen))  # 取第二个值
 print(next(gen))  # 取第三个值
-print(next(gen))  # 取第不存在的第四个值，抛出异常
+print(next(gen))  #取不存在的第四个值，会抛出StopIteration异常
