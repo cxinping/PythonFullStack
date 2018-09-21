87a30dc12d9047782d9de4e5848d903080bb4808
xpws2006@163.com <123@welcome>
2018-09-22 01:49:02 +0800

    修改表中记录

--- Chapter04/mysql_update.py
+++ Chapter04/mysql_update.py
@@ -2,7 +2,8 @@
 
 import pymysql
 # 打开数据库连接
-db = pymysql.connect("127.0.0.1", "root", "123456", "mytestdb",charset='utf8')
+db = pymysql.connect(host="127.0.0.1", user="root", password="123456",
+                     database="mytestdb", port=3306, charset="utf8")
 
 # 使用cursor()方法获取操作游标
 cursor = db.cursor()
@@ -15,9 +16,9 @@ try:
     # 提交到数据库执行
     db.commit()
 except Exception as e:
-    print( e )
+    print(e)
     # 发生错误时回滚
     db.rollback()
-
-# 关闭数据库连接
-db.close()
+finally:
+    # 关闭数据库连接
+    db.close()
