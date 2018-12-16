098fad3802c198017533dc6ff4bf6344909a8bc1
xpws2006@163.com <123@welcome>
2018-12-16 14:20:41 +0800

    优化带附件的发送邮件代码

--- Chapter03/sendEmail03.py
+++ Chapter03/sendEmail03.py
@@ -7,11 +7,12 @@ from email.header import Header
 
 # 第三方 SMTP 服务
 mail_host="smtp.163.com"  #设置服务器
-mail_user= "xinpingedu@163.com"   #用户名
-mail_pass= "123welcome"   #口令
+mail_user="xinpingedu@163.com"    #用户名
+mail_pass="123welcome"   #口令
 
-sender = mail_pass
+sender = mail_user
 receivers = [ 'xpws2006@163.com' ]  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
 
 #创建一个带附件的实例
 message = MIMEMultipart()
